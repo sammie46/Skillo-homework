c293539a0df5b1d5fb4bb30658c4959965bef10d
sammie46 <160309268+sammie46@users.noreply.github.com>
2025-07-27 00:19:40 +0300

    Update names.java

--- homework 1/names.java
+++ homework 1/names.java
@@ -5,10 +5,10 @@ public class Main {
  Scanner scanner = new Scanner(System.in);
  //Without input
         String firstName ="Betty";
-        System.out.println("Your first name is "+firstName);
         String midName ="Netty";
-        System.out.println("Your middle name is "+midName);
         String surname ="Ketty";
+        System.out.println("Your first name is "+firstName);
+        System.out.println("Your middle name is "+midName);
         System.out.println("Your surname: "+surname);
         System.out.println("Your full name is "+firstName+ midName+ surname);
    
