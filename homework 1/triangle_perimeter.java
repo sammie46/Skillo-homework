192e80aab25f642339bbc633b6f298daa9b78f32
sammie46 <160309268+sammie46@users.noreply.github.com>
2025-07-27 00:00:01 +0300

    Create triangle_perimeter.java

---
+++ homework 1/triangle_perimeter.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+ Scanner scanner = new Scanner(System.in);
+
+      //Without input
+       System.out.println("Triangle perimeter");
+        float firstSide = 5;
+        System.out.println("The first side is "+firstSide);
+        float secondSide = 6;
+        System.out.println("The second side is "+secondSide);
+        float thirdSide = 8;
+        System.out.println("The third side: "+thirdSide);
+        float perimeter = firstSide+secondSide+thirdSide;
+        System.out.println("The perimeter of the triangle is: "+perimeter);  
+     
+     //With input 
+      /* System.out.println("Triangle perimeter");
+        System.out.println("Enter the first side: ");
+        float firstSide = scanner.nextFloat();
+        System.out.println("Enter the second side: ");
+        float secondSide = scanner.nextFloat();
+        System.out.println("Enter the third side: ");
+        float thirdSide = scanner.nextFloat();
+        float perimeter = firstSide+secondSide+thirdSide;
+        System.out.println("The perimeter of the triangle is: "+perimeter);
+*/
+
